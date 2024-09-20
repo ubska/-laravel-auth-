935744ec867a65af8480aa3ee0d8394bf33c22c7
ubska <alessandraubska@gmail.com>
2024-09-20 17:30:08 +0200

    feat: guest page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        dump('sono index della dashboard!!!!!!');
+        return view('admin.index');
+        // dump('sono index della dashboard!!!!!!');
     }
 }
